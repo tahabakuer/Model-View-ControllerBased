69f67dd14f675aa3cb4530f4ec9d91408a33f149
tahabakuer <80453477+tahabakuer@users.noreply.github.com>
2023-09-27 12:43:30 +0300

    Initial commit

---
+++ Model-View-ControllerBased/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Model_View_ControllerBased
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Model-View-ControllerBased/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Model_View_ControllerBased.MvcApplication" Language="C#" %>
