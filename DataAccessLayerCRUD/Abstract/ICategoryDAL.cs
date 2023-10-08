f1930d99684b34444bb00b1ab2b5c8a3d0384422
tahabakuer <80453477+tahabakuer@users.noreply.github.com>
2023-10-08 09:25:24 +0300

    Category Interface for DAL
    Category Interface method is created

---
+++ DataAccessLayerCRUD/Abstract/ICategoryDAL.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerCRUD.Abstract
+{
+    public interface ICategoryDAL
+    {
+        //CRUD Operation Handling
+        List<Category> List();
+        void Insert(Category p); //p Category param
+
+        void Update(Category p);
+
+        void Delete(Category p);
+    }
+}
