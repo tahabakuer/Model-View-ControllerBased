1d75e98b812b818f18d5d241af7c9f9bda0aa3b9
tahabakuer <80453477+tahabakuer@users.noreply.github.com>
2023-10-08 09:49:47 +0300

    DAL CRUD
    DAL repo created for CRUD
    Created IRepository to abstracts folder
    DAL>Concrete>Repositories

---
+++ DataAccessLayerCRUD/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerCRUD.Abstract
+{
+    public interface IRepository<T>// T is Table param
+    {
+        //CRUD
+        List<T> List();
+        void Insert(T p); //p Tables param
+
+        void Update(T p);
+
+        void Delete(T p);
+    }
+}
---
+++ DataAccessLayerCRUD/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayerCRUD.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerCRUD.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDAL
+    {
+        Context c= new Context();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();//this is a must for save
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();// i will add updates before this func will work so its just need to save
+        }
+    }
+}
+
+/*
+ * ToList
+ * Add
+ * Remove
+ * Find
+ * these are basic entity framework functions
+ */
